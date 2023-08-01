df99a52eaba9834feef3ffd73687fd7b071edc93
nischal gautam <nischalgautam7777@gmail.com>
2023-08-01 21:51:41 +0530

    tried creating objects of each cell and improved the looks using css

---
+++ index.js
@@ -0,0 +1,36 @@
+const gridElement0 = document.getElementById('cell0');
+const gridElement1 = document.getElementById('cell1');
+const gridElement2 = document.getElementById('cell2');
+const gridElement3 = document.getElementById('cell3');
+const gridElement4 = document.getElementById('cell4');
+const gridElement5 = document.getElementById('cell5');
+const gridElement6 = document.getElementById('cell6');
+const gridElement7 = document.getElementById('cell7');
+
+const cell = (domIndex)=>{
+    let content = ' ';
+    const changeContent = ()=>document.getElementById(domIndex).innerText=content;
+    return {domIndex,changeContent};
+
+}
+
+
+
+
+
+
+ gameboard = () =>{
+    let tictaccell = [];
+}
+
+
+function render(){
+    let gameboardhtml = `
+
+    
+    
+    
+`;
+    
+
+}
