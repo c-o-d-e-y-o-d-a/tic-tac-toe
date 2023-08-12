b0b418d4f93cddf5c7594f06d76b2e6e5d6adab7
nischal gautam <nischalgautam7777@gmail.com>
2023-08-12 19:40:59 +0530

    made factory functions to represent the cells in the game,and a gameboard factory functions and a render method to display the gameboard

--- index.js
+++ index.js
@@ -1,69 +1,55 @@
-const cell = (domIndex)=>{
+const cell = (valueOfDom) =>{
+    let ifCellIsMarked = false;
+    let cellContent = " ";
+    const changeCellContent = (content)=>{
+        cellContent = content;
+        ifCellIsMarked = true;
+    };
 
-    
-    const changeContent = (newContent)=>document.getElementById(domIndex).innerText=newContent;
-    return {domIndex,changeContent,};
+    let indexOfCell = '';
+    return{ifCellIsMarked,cellContent,changeCellContent,valueOfDom,indexOfCell};
+};
+  
 
-}
+const GameBoard = (DomOfGameBoard)=>{
+    let isGameOver = false;
+    let cellStorage  ={};
+    const playerMove = (index,content)=>{
+        if(isGameOver===false){
+            cellStorage[index].changeCellContent(content);
+            render();
 
-//making objects of each cell
-
-
-const gameBoardObject =()=>{
-    const gridElement0 = cell("cell0");
-    const gridElement1 = cell("cell1");
-    const gridElement2 = cell("cell2");
-    const gridElement3 = cell("cell3");
-    const gridElement4 = cell("cell4");
-    const gridElement5 = cell("cell5");
-    const gridElement6 = cell("cell6");
-    const gridElement7 = cell("cell7");
-    let gameBoardCellList = [gridElement0,gridElement1,gridElement2,gridElement3,gridElement4,gridElement5,gridElement5,gridElement6,gridElement7];
-    
-
-}
-let player = 1;
-
-
-const elem = document.getElementsByClassName('tic_tac_toe_cell');
-for(let i=0;i<elem.length;i++){
-    elem[i].addEventListener('click',()=>{
-        if(player==1){
-            elem[i].innerText='X';
-            player=2;
-            console.log('X');
-            // add a function here to check if at certainindexes the elements are same or not (to finish the game)
-            
-            
+        if(cellStorage[0].ifCellIsMarked==true && cellStorage[1].ifCellIsMarked==true && cellStorage[2].ifCellIsMarked==true && cellStorage[3].ifCellIsMarked==true && cellStorage[4].ifCellIsMarked==true && cellStorage[5].ifCellIsMarked==true && cellStorage[6].ifCellIsMarked==true && cellStorage[7].ifCellIsMarked==true ){
+            isGameOver=true;
         }
-        else if(player==2){
-            elem[i].innerText='O';
-            player=1;
-            console.log('O');
+        if(cellStorage[0]==cellStorage[3]==cellStorage[4] || cellStorage[1]==cellStorage[4]==cellStorage[7] || cellStorage[2]==cellStorage[5]==cellStorage[8] || cellStorage[0]==cellStorage[1]==cellStorage[2] || cellStorage[3]==cellStorage[4]==cellStorage[5] || cellStorage[6]==cellStorage[7]==cellStorage[8] || cellStorage[0]==cellStorage[4]==cellStorage[8] || cellStorage[2]==cellStorage[4]==cellStorage[8]){
+            isGameOver=true;
+            console.log("the game is over");
         }
-    })
-}
+        
+        
 
 
+    };
+    return{isGameOver,cellStorage,playerMove,DomOfGameBoard};
 
-
-
-
-
-
-
-
-
- 
-
+};
+};
 
 function render(){
-    let gameboardhtml = `
-
-    
-    
-    
-`;
-    
-
+    document.querySelector('.gameContainer').innerHTML='';
+    let htmlOfGameBoard=`
+    <div class="tic_tac_toe_cell" id="cell1">${GameBoard.cellStorage[0].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell3">${GameBoard.cellStorage[1].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell2">${GameBoard.cellStorage[2].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell4">${GameBoard.cellStorage[3].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell5">${GameBoard.cellStorage[4].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell6">${GameBoard.cellStorage[5].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell7">${GameBoard.cellStorage[6].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell8">${GameBoard.cellStorage[7].cellContent}</div>
+    <div class="tic_tac_toe_cell" id="cell0">${GameBoard.cellStorage[8].cellContent}</div>
+    `;
+    document.querySelector('.gameContainer').innerHTML=htmlOfGameBoard;
 }
+
+
