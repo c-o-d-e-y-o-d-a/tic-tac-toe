040e4442e3663199d5b523a401a039c6a4821b6b
nischal gautam <nischalgautam7777@gmail.com>
2023-08-18 20:50:46 +0530

    improved the js part of the project

--- index.js
+++ index.js
@@ -1,55 +1,86 @@
-const cell = (valueOfDom) =>{
+const cell = (DomPointer) => {
     let ifCellIsMarked = false;
     let cellContent = " ";
-    const changeCellContent = (content)=>{
+    
+    const changeCellContent = (content) => {
         cellContent = content;
         ifCellIsMarked = true;
     };
 
-    let indexOfCell = '';
-    return{ifCellIsMarked,cellContent,changeCellContent,valueOfDom,indexOfCell};
+    return { ifCellIsMarked, cellContent, changeCellContent };
 };
-  
 
-const GameBoard = (DomOfGameBoard)=>{
+const GameBoard = (DomOfGameBoard) => {
     let isGameOver = false;
-    let cellStorage  ={};
-    const playerMove = (index,content)=>{
-        if(isGameOver===false){
-            cellStorage[index].changeCellContent(content);
-            render();
+    let cellStorage = [];
 
-        if(cellStorage[0].ifCellIsMarked==true && cellStorage[1].ifCellIsMarked==true && cellStorage[2].ifCellIsMarked==true && cellStorage[3].ifCellIsMarked==true && cellStorage[4].ifCellIsMarked==true && cellStorage[5].ifCellIsMarked==true && cellStorage[6].ifCellIsMarked==true && cellStorage[7].ifCellIsMarked==true ){
-            isGameOver=true;
+    for (let i = 0; i < 9; i++) {
+        cellStorage.push(cell(document.getElementById(`cell${i}`)));
+    }
+
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]            // Diagonals
+    ];
+
+    const checkWinningConditions = (symbol) => {
+        for (const combo of winningCombinations) {
+            const [a, b, c] = combo;
+            if (cellStorage[a].cellContent === symbol &&
+                cellStorage[b].cellContent === symbol &&
+                cellStorage[c].cellContent === symbol) {
+                return true;
+            }
         }
-        if(cellStorage[0]==cellStorage[3]==cellStorage[4] || cellStorage[1]==cellStorage[4]==cellStorage[7] || cellStorage[2]==cellStorage[5]==cellStorage[8] || cellStorage[0]==cellStorage[1]==cellStorage[2] || cellStorage[3]==cellStorage[4]==cellStorage[5] || cellStorage[6]==cellStorage[7]==cellStorage[8] || cellStorage[0]==cellStorage[4]==cellStorage[8] || cellStorage[2]==cellStorage[4]==cellStorage[8]){
-            isGameOver=true;
-            console.log("the game is over");
-        }
-        
-        
-
-
+        return false;
     };
-    return{isGameOver,cellStorage,playerMove,DomOfGameBoard};
 
-};
+    const playerMove = (index, content) => {
+        if (!isGameOver && cellStorage[index].cellContent === " ") {
+            cellStorage[index].changeCellContent(content);
+            render(cellStorage);
+
+            if (checkWinningConditions(content)) {
+                isGameOver = true;
+                console.log(`${content} wins!`);
+            }
+
+            if (cellStorage.every(cell => cell.ifCellIsMarked)) {
+                isGameOver = true;
+                console.log("It's a draw!");
+            }
+        }
+    };
+
+    return { isGameOver, cellStorage, playerMove, DomOfGameBoard };
 };
 
-function render(){
-    document.querySelector('.gameContainer').innerHTML='';
-    let htmlOfGameBoard=`
-    <div class="tic_tac_toe_cell" id="cell1">${GameBoard.cellStorage[0].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell3">${GameBoard.cellStorage[1].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell2">${GameBoard.cellStorage[2].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell4">${GameBoard.cellStorage[3].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell5">${GameBoard.cellStorage[4].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell6">${GameBoard.cellStorage[5].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell7">${GameBoard.cellStorage[6].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell8">${GameBoard.cellStorage[7].cellContent}</div>
-    <div class="tic_tac_toe_cell" id="cell0">${GameBoard.cellStorage[8].cellContent}</div>
-    `;
-    document.querySelector('.gameContainer').innerHTML=htmlOfGameBoard;
+function render(cellStorage) {
+    document.querySelector('.gameContainer').innerHTML = '';
+
+    let htmlOfGameBoard = '';
+    for (let i = 0; i < 9; i++) {
+        htmlOfGameBoard += `<div class="tic_tac_toe_cell" id="cell${i}">${cellStorage[i].cellContent}</div>`;
+    }
+
+    document.querySelector('.gameContainer').innerHTML = htmlOfGameBoard;
 }
 
+const gameContainer = document.querySelector('.gameContainer');
+const game = GameBoard(gameContainer);
+
+const cellElements = document.querySelectorAll('.tic_tac_toe_cell');
+let playerIdentifier = 0; 
+
+cellElements.forEach((cell, index) => {
+    cell.addEventListener('click', () => {
+        if (!game.isGameOver) {
+            const playerSymbol = playerIdentifier === 0 ? 'X' : 'O';
+            game.playerMove(index, playerSymbol);
+
+            playerIdentifier = 1 - playerIdentifier;
+        }
+    });
+});
 
